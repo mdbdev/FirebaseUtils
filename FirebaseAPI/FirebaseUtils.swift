63b15f89946b9f48c82fa52e2d5fce236dbabbd0
Christine Munar <chrismunar@berkeley.edu>
2016-11-20 03:12:02 -0800

    Fixed up some logic behind the methods

--- FirebaseAPI/FirebaseUtils.swift
+++ FirebaseAPI/FirebaseUtils.swift
@@ -12,9 +12,10 @@ import Firebase
 class FirebaseUtils {
     
     /** 
-     * queryById
+     * queryById 
+     * - ASYNC
      *
-     * @params id (String), ref (FIRDatabaseReference)
+     * @params id (String), ref (FIRDatabaseReference), childType (AnyClass) // FIGURE OUT TYPE THING
      * @return JSON object with id as its parent
      */
     func queryById(id : String, ref : FIRDatabaseReference) { // NEED TO FIGURE OUT TYPE THINGY
@@ -39,22 +40,37 @@ class FirebaseUtils {
     /**
      * queryByFieldContains: compares values for each FIELDNAME to see
      * they contain the given VALUE
+     *  - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValue (NSObject)
      * @return array of children that contain VALUE
      */
-    func queryByFieldContains(fieldName : String, ref : FIRDatabaseReference, inputValue : NSObject) {
+    func queryByFieldContains(fieldName : String, ref : FIRDatabaseReference, inputValue : String) {
         ref.child(fieldName).observeSingleEvent(of: .value, with: { (snapshot) in
             if (!snapshot.exists()) {
                 // REPLACE THIS WITH RETURN SOMETHING NIL
             } else {
                 var results : [NSObject] = []
-                for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if snapshotValue.contains(inputValue) {
+                
+                // THE CODE BELOW I'M NOT SURE OF
+                // IM UNCLEAR IF THIS FUNCTION WANTS TO CHECK THE CHILDREN OF THE SNAPSHOT
+                // OR IF IT WANTS TO CHECK IF VALUE PROPERTIES EXIST
+                
+                let enumerator = snapshot.children
+                while let snapshotValue = enumerator.nextObject() as? FIRDataSnapshot {
+                    if snapshotValue.hasChild(inputValue) {
                         results.append(snapshotValue)
                     }
                 }
+                
+                
+//                for childSnapshot in snapshot.children {
+//                    let snapshotValue = childSnapshot.value as! NSDictionary
+//                    if snapshotValue.contains(inputValue) {
+//                        results.append(snapshotValue)
+//                    }
+//                }
+                
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
         }) { (error) in
@@ -66,6 +82,7 @@ class FirebaseUtils {
     /**
      * queryByListContains: compares values for each FIELDNAME to see
      * if that value is contained in the list of INPUTVALUES
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValues (NSArray)
      * @return array of children that contain VALUE
@@ -77,10 +94,10 @@ class FirebaseUtils {
             } else {
                 var results : [NSObject] = []
                 for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if inputValues.contains(snapshotValue) {
-                        results.append(snapshotValue)
-                    }
+//                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
+//                    if inputValues.contains(snapshotValue) {
+//                        results.append(snapshotValue)
+//                    }
                 }
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
@@ -93,6 +110,7 @@ class FirebaseUtils {
     /**
      * queryByEqualTo: compare values for each FIELDNAME
      * to see if that value is equal to INPUTVALUE
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValue (NSObject)
      * @return array of children that are equal to the INPUTVALUE
@@ -104,10 +122,10 @@ class FirebaseUtils {
             } else {
                 var results : [NSObject] = []
                 for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if snapshotValue.equals(inputValue) {
-                        results.append(snapshotValue)
-                    }
+//                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
+//                    if snapshotValue.equals(inputValue) {
+//                        results.append(snapshotValue)
+//                    }
                 }
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
@@ -119,6 +137,7 @@ class FirebaseUtils {
     /**
      * queryByNotEqualTo: compare values for each FIELDNAME
      * to see if that value is not equal to INPUTVALUE
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValue (NSObject)
      * @return array of children that are not equal to the INPUTVALUE
@@ -130,10 +149,10 @@ class FirebaseUtils {
             } else {
                 var results : [NSObject] = []
                 for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if !(snapshotValue.equals(inputValue)) {
-                        results.append(snapshotValue)
-                    }
+//                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
+//                    if !(snapshotValue.equals(inputValue)) {
+//                        results.append(snapshotValue)
+//                    }
                 }
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
@@ -145,6 +164,7 @@ class FirebaseUtils {
     /**
      * queryByLessThan: compare values for each FIELDNAME
      * to see if that value is less than INPUTVALUE
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValue (NSObject)
      * @return array of children that are less than the INPUTVALUE
@@ -156,10 +176,10 @@ class FirebaseUtils {
             } else {
                 var results : [NSObject] = []
                 for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if (snapshotValue < inputValue) {
-                        results.append(snapshotValue)
-                    }
+//                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
+//                    if (snapshotValue < inputValue) {
+//                        results.append(snapshotValue)
+//                    }
                 }
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
@@ -172,6 +192,7 @@ class FirebaseUtils {
     /**
      * queryByGreaterThan: compare values for each FIELDNAME
      * to see if that value is greater than INPUTVALUE
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), inputValue (NSObject)
      * @return array of children that are greater than the INPUTVALUE
@@ -183,10 +204,10 @@ class FirebaseUtils {
             } else {
                 var results : [NSObject] = []
                 for childSnapshot in snapshot.children {
-                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
-                    if (snapshotValue > inputValue) {
-                        results.append(snapshotValue)
-                    }
+//                    let snapshotValue = (childSnapshot as AnyObject).value as! NSDictionary
+//                    if (snapshotValue > inputValue) {
+//                        results.append(snapshotValue)
+//                    }
                 }
                 // FIGURE OUT HOW TO RETURN RESULTS ARRAY
             }
@@ -199,14 +220,22 @@ class FirebaseUtils {
     /**
      * queryByFileEquals: compare urls for each FIELDNAME, 
      * downloads the file at that url in bytes, and then compares those bytes to FILEBYTES
+     * - ASYNC
      *
      * @params fieldName (String), ref (FIRDatabaseReference), fileBytes (???)
      * @return ??? NOT SURE WHAT THIS RETURNS
      */
     func queryByFileEquals(fieldName : String, ref : FIRDatabaseReference, fileBytes : NSData) {
-        // NOT SURE THE TYPE OF THE FILEBYTES TO BE PASSED IN --> NSDATA OR BYTE[] OR ????
-        // NO IDEA HOW TO D THIS
+        ref.child(fieldName).observeSingleEvent(of: .value, with: { (snapshot) in
+            if (!snapshot.exists()) {
+                // REPLACE THIS WITH RETURN SOMETHING NIL
+            } else {
+                // NOT SURE THE TYPE OF THE FILEBYTES TO BE PASSED IN --> NSDATA OR BYTE[] OR ????
+                // NO IDEA HOW TO Do THIS
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
-    
-    
 }
